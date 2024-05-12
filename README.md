f2cea1ef6e7d53c32b2990c8c55d351c8d3926da
Arman Hossain <arman.hossain.bd93@gmail.com>
2024-05-12 03:01:09 +0200

    updated

--- README.md
+++ README.md
@@ -1,35 +1,43 @@
-# RPL Simulation
+# RPL Protocol Simulation
 
-This project simulates the Routing over Low Power and Lossy Networks (RPL) protocol using the SimPy discrete event simulation framework. The simulation is designed to visualize the Dynamic Objective Directed Acyclic Graph (DODAG) formation process using RPL's DIS, DIO, and DAO messaging mechanisms.
+This repository contains the Python code for simulating the Routing over Low Power and Lossy Networks (RPL) protocol. The simulation uses SimPy, a discrete-event simulation framework, to model the behavior of nodes within a low-power lossy network, specifically focusing on the dynamics of RPL's routing mechanisms.
 
-## Overview
+## Features
 
-The simulation models a network of nodes distributed within a defined area. Each node attempts to discover neighbors and build a routing structure based on the RPL protocol specifications. The key features demonstrated include neighbor discovery, DODAG formation, and network prefix distribution using DAO messages.
-
-## Simulation Details
-
-- **Random Seed**: Customizable to achieve different random node distributions.
-- **Number of Nodes**: 50
-- **Area Dimensions**: 125m x 125m
-- **Connection Range**: Each node has a connection range of 25 meters.
-- **DIO Interval**: DIO messages are sent every 5 seconds.
-- **Node Creation Interval**: Nodes are created with a 1-second interval between each to simulate gradual network population.
-
-### Key Components
-
-- **Node**: Represents a network device participating in the RPL network.
-  - Each node broadcasts a DIS message upon creation to discover neighbors.
-  - Nodes respond to DIS messages from other nodes within their connection range with a DIO message.
-  - The network topology is established based on the reception of DIO messages, where nodes select their parent to form the DODAG structure.
-  - DAO messages are used to propagate routing prefix information up the DODAG.
-
-- **Trickle Algorithm**: Implemented to manage the timing of DIS messages, increasing efficiency and reducing network chatter.
+- **Neighbor Discovery**: Nodes dynamically discover neighbors based on a specified connection range.
+- **DODAG Formation**: Implements the RPL's DODAG (Directed Acyclic Graph) formation using DIO (DODAG Information Object) messages.
+- **Prefix Distribution**: Utilizes DAO (Destination Advertisement Object) messages for the distribution of network prefixes.
+- **Trickle Algorithm**: Controls the timing of DIS (DODAG Information Solicitation) messages to optimize network traffic.
+- **Network Disruptions**: Simulates network disruptions and node recovery to test the robustness of the network.
 
 ## Requirements
 
 - Python 3.6 or higher
 - SimPy
 - Matplotlib
+
+## Installation
+
+### Clone the repository:
+```bash
+git clone https://github.com/arman-bd/rpl-simulation.git
+```
+
+### Install the required packages:
+```bash
+pip install simpy matplotlib
+```
+
+## Configuration
+Modify the simulation parameters in `simulation.py` according to your needs. You can modify default values directly in code or when launching the script.
+
+- **RANDOM_SEED**: Random seed for reproducibility.
+- **NUM_NODES**: Number of nodes in the simulation.
+- **AREA_WIDTH, AREA_HEIGHT**: Dimensions of the simulation area.
+- **CONNECTION_RANGE**: Maximum range for connecting to other nodes.
+- **DIO_INTERVAL**: Interval at which DIO messages are sent.
+- **NODE_CREATION_INTERVAL**: Interval at which nodes are created in the simulation.
+- **RUNTIME**: Duration of the simulation.
 
 ## Usage
 
@@ -39,12 +47,10 @@ To run the simulation, execute the script `simulation.py`:
 python simulation.py
 ```
 
-The simulation will generate a plot showing the node distribution and the DODAG connections. Additionally, diagnostic outputs including node creation, neighbor discovery, and message exchanges are printed to the console and saved to a file named simulation.txt.
+The simulation will generate a plot showing the node distribution and the DODAG connections. Additionally, diagnostic outputs including node creation, neighbor discovery, and message exchanges are printed to the console and saved to a file named output.txt.
 
 ## Visualization
 The generated plot will illustrate nodes as points on a grid with lines connecting them to represent parent-child relationships in the DODAG. Each node's ID and the IPv6 prefix are displayed next to the corresponding node on the plot.
-
-
 
 ## Output
 * A PNG image of the node distribution and connections named output.png.
@@ -55,21 +61,31 @@ The generated plot will illustrate nodes as points on a grid with lines connecti
 <img src="output.png">
 
 ```text
-RPL Simulation
-0.00 Node00 created at (2.654, 9.3297)
-0.00 Node00 sends initial DIS broadcast for neighbor discovery
-0.00 Node00 sends DIO to neighbors
-1.00 Node01 created at (29.1376, 47.7352)
-1.00 Node01 sends initial DIS broadcast for neighbor discovery
-1.00 Node01 sends DIO to neighbors
-2.00 Node02 created at (75.3866, 114.8086)
-2.00 Node02 sends initial DIS broadcast for neighbor discovery
-2.00 Node02 sends DIO to neighbors
-3.00 Node03 created at (0.4701, 116.6502)
-3.00 Node03 sends initial DIS broadcast for neighbor discovery
-3.00 Node03 sends DIO to neighbors
-4.00 Node04 created at (119.0886, 1.7474)
-4.00 Node04 sends initial DIS broadcast for neighbor discovery
+0.00 Node00 created at (103.3963, 26.5954)
+0.00 Node00 starting neighbor discovery
+1.00 Node01 created at (21.2037, 24.2015)
+1.00 Node01 starting neighbor discovery
+1.00 Node00 sends DIS broadcast for neighbor discovery
+2.00 Node02 created at (27.5986, 89.5239)
+2.00 Node02 starting neighbor discovery
+2.00 Node01 sends DIS broadcast for neighbor discovery
+2.28 Node00 sends DIS broadcast for neighbor discovery
+3.00 Node03 created at (108.8647, 95.1618)
+3.00 Node03 starting neighbor discovery
+3.00 Node02 sends DIS broadcast for neighbor discovery
+3.57 Node01 sends DIS broadcast for neighbor discovery
+4.00 Node04 created at (78.3705, 118.4513)
+4.00 Node04 starting neighbor discovery
+4.00 Node03 sends DIS broadcast for neighbor discovery
+4.50 Node00 sends DIS broadcast for neighbor discovery
+4.64 Node01 sends DIS broadcast for neighbor discovery
+4.94 Node02 sends DIS broadcast for neighbor discovery
+5.00 Node05 created at (21.0677, 121.0056)
+5.00 Node05 starting neighbor discovery
+5.00 Node04 sends DIS broadcast for neighbor discovery
+5.11 Node03 sends DIS broadcast for neighbor discovery
+6.00 Node06 created at (85.9161, 59.6546)
+6.00 Node06 starting neighbor discovery
 ...
 ...
 ```
--- simulation.py
+++ simulation.py
@@ -4,153 +4,187 @@ import random
 import simpy
 import matplotlib.pyplot as plt
 
-# Delete simulation.txt file if exists
-if os.path.exists('output.txt'):
-    os.remove('output.txt')
+# Delete output files if they exist
+for filename in ['output.txt', 'output.png']:
+    if os.path.exists(filename):
+        os.remove(filename)
 
-# Redirect print to a File
-def print(*args, **kwargs):
+# Function to log messages to a file and print them to stdout
+def log(*args, **kwargs):
     with open('output.txt', 'a') as f:
         builtins.print(*args, **kwargs, file=f)
     builtins.print(*args, **kwargs)
 
-# Simulation parameters
-RANDOM_SEED = 442 # Seed for random number generation
-NUM_NODES = 50  # Total number of nodes
-AREA_WIDTH = 125  # Meters
-AREA_HEIGHT = 125  # Meters
-MINIMUM_DISTANCE = 5  # Minimum distance between nodes
-CONNECTION_RANGE = 25  # Maximum range for connecting to other nodes
-DIO_INTERVAL = 5  # Time between DIO messages in seconds
-NODE_CREATION_INTERVAL = 1  # Delay in seconds between node creations
+# Function to configure simulation parameters
+def configure_simulation():
+    print("Using default simulation parameters")
+    config = {
+        "RANDOM_SEED": 111,
+        "NUM_NODES": 50,
+        "AREA_WIDTH": 125,
+        "AREA_HEIGHT": 125,
+        "MINIMUM_DISTANCE": 5,
+        "CONNECTION_RANGE": 20,
+        "DIO_INTERVAL": 10,
+        "NODE_CREATION_INTERVAL": 1,
+        "RUNTIME": 120
+    }
+    configure = input("Do you want to configure simulation parameters? y/[N]: ")
+    if configure.lower() == 'y':
+        config["RANDOM_SEED"] = int(input("Enter Random Seed [111]: ") or config["RANDOM_SEED"])
+        config["NUM_NODES"] = int(input("Enter Number of Nodes [50]: ") or config["NUM_NODES"])
+        config["AREA_WIDTH"] = int(input("Enter Area Width in Meters [125]: ") or config["AREA_WIDTH"])
+        config["AREA_HEIGHT"] = int(input("Enter Area Height in Meters [125]: ") or config["AREA_HEIGHT"])
+        config["MINIMUM_DISTANCE"] = int(input("Enter Minimum Distance Between Nodes in Meters [5]: ") or config["MINIMUM_DISTANCE"])
+        config["CONNECTION_RANGE"] = int(input("Enter Connection Range in Meters [15]: ") or config["CONNECTION_RANGE"])
+        config["DIO_INTERVAL"] = int(input("Enter DIO Interval in Seconds [10]: ") or config["DIO_INTERVAL"])
+        config["NODE_CREATION_INTERVAL"] = int(input("Enter Node Creation Interval in Seconds [1]: ") or config["NODE_CREATION_INTERVAL"])
+        config["RUNTIME"] = int(input("Enter Simulation Runtime in Seconds [120]: ") or config["RUNTIME"])
+    return config
 
-random.seed(RANDOM_SEED + random.randint(0, 1000))
+# Initialize random seed
+config = configure_simulation()
+random.seed(config["RANDOM_SEED"])
 
 class Node:
     def __init__(self, env, node_id, position, all_nodes):
+        # Environment
         self.env = env
+
+        # Node Information
+        self.parent = None
         self.node_id = node_id
         self.position = position
         self.all_nodes = all_nodes
         self.neighbors = []
-        self.parent = None
-        # Create a unique prefix for the node
+        self.lost_neighbors = []
+
+        # Network Prefix
         self.prefix = f'2001:db8::{int(node_id[4:]):02x}'
-        # Trickle parameters
+
+        # Trickle Parameters
         self.Imin = 1
         self.Imax = 10
         self.I = self.Imin
         self.t = random.uniform(self.Imin, self.I)
 
-    def _discover_neighbors(self):
-        print(f'{self.env.now:.2f} {self.node_id} created at {self.position} and starting neighbor discovery')
-        for node in self.all_nodes:
-            if node != self:
-                distance = self.calculate_distance(node.position)
-                if distance <= CONNECTION_RANGE:
-                    self.neighbors.append(node)
-                    print(f'{self.env.now:.2f} {self.node_id} discovered neighbor {node.node_id} at {distance:.2f} meters')
-                    # draw a line between the nodes
-                    plt.plot([self.position[0], node.position[0]], [self.position[1], node.position[1]], color='blue', linestyle='--', linewidth=0.5)
-        yield self.env.timeout(0)
+        # Color for Plotting
+        self.color = (random.random(), random.random(), random.random())
 
     def discover_neighbors(self):
         # Initiate neighbor discovery process
-        print(f'{self.env.now:.2f} {self.node_id} sends initial DIS broadcast for neighbor discovery')
+        log(f'{self.env.now:.2f} {self.node_id} starting neighbor discovery')
         yield self.env.timeout(0.1)  # Simulate delay for sending DIS
         for node in self.all_nodes:
-            if node != self and self.calculate_distance(node.position) <= CONNECTION_RANGE:
+            if node != self and self.calculate_distance(node.position) <= config["CONNECTION_RANGE"]:
                 self.env.process(node.receive_dis(self))
+                log(f'{self.env.now:.2f} {self.node_id} sends DIS to {node.node_id}')
 
     def send_dio(self):
+        # Send DIO message to all neighbors
         while True:
-            print(f'{self.env.now:.2f} {self.node_id} sends DIO to neighbors')
             for neighbor in self.neighbors:
                 self.env.process(neighbor.receive_dio(self))
-            yield self.env.timeout(DIO_INTERVAL)
+                log(f'{self.env.now:.2f} {self.node_id} sends DIO to {neighbor.node_id}')
+            yield self.env.timeout(config["DIO_INTERVAL"])
     
     def receive_dio(self, sender):
+        # Receive DIO message from a neighbor
         if not self.parent:
             self.parent = sender
-            print(f'{self.env.now:.2f} {self.node_id} received DIO from {sender.node_id} and sets as parent')
             # Update network prefix
             self.prefix = f'{sender.prefix}:{int(self.node_id[4:]):02x}'
-            print(f'{self.env.now:.2f} {self.node_id} updated prefix to {self.prefix}')
-            # Draw a line between the nodes
-            plt.plot([self.position[0], sender.position[0]], [self.position[1], sender.position[1]], color='blue', linestyle='--', linewidth=0.5)
-            self.send_dao()
-        yield self.env.timeout(0)
+            yield self.env.process(self.send_dao())
+            log(f'{self.env.now:.2f} {self.node_id} received DIO from {sender.node_id}; new parent: {sender.node_id}; new prefix: {self.prefix}')
+        else:
+            # Check if sender is a Closer parent
+            if self.calculate_distance(sender.position) < self.calculate_distance(self.parent.position):
+                # Update network prefix
+                self.prefix = f'{sender.prefix}:{int(self.node_id[4:]):02x}'
+                self.parent = sender
+                yield self.env.process(self.send_dao())
+                log(f'{self.env.now:.2f} {self.node_id} received DIO from {sender.node_id}; new closer parent: {sender.node_id}; new prefix: {self.prefix}')
 
-    def _send_dis(self):
-        if not self.neighbors:
-            print(f'{self.env.now:.2f} {self.node_id} sends DIS due to no neighbors')
-            for node in self.all_nodes:
-                if node != self:
-                    self.env.process(node.receive_dis(self))
-            self.reset_trickle()
-        yield self.env.timeout(0.1)
+        yield self.env.timeout(0)
     
     def send_dis(self):
-        # Send DIS message to all nodes in the range
         if not self.neighbors:
-            print(f'{self.env.now:.2f} {self.node_id} sends DIS due to no neighbors')
             for node in self.all_nodes:
-                if node != self and self.calculate_distance(node.position) <= CONNECTION_RANGE:
+                if node != self and self.calculate_distance(node.position) <= config["CONNECTION_RANGE"]:
                     self.env.process(node.receive_dis(self))
-            self.reset_trickle()
-        yield self.env.timeout(0.1)
-
-    def _receive_dis(self, sender):
-        print(f'{self.env.now:.2f} {self.node_id} received DIS from {sender.node_id} and sends DIO')
-        self.env.process(sender.receive_dio(self))
-        yield self.env.timeout(0.1)
+        yield self.env.timeout(0)
 
     def receive_dis(self, sender):
         # Receive DIS message from a neighbor and send DIO message
         distance = self.calculate_distance(sender.position)
-        if sender not in self.neighbors and distance <= CONNECTION_RANGE:
-            self.neighbors.append(sender)
-            print(f'{self.env.now:.2f} {self.node_id} received DIS from {sender.node_id}, sends DIO')
+        if sender not in self.neighbors and distance <= config["CONNECTION_RANGE"]:
+            self.neighbors.append(sender) if sender not in self.neighbors else None
+            sender.neighbors.append(self) if self not in sender.neighbors else None
             self.env.process(sender.receive_dio(self))
+            log(f'{self.env.now:.2f} {self.node_id} received DIS from {sender.node_id}; new neighbor: {sender.node_id}')
         yield self.env.timeout(0.1)
 
     def send_dao(self):
-        print(f'{self.env.now:.2f} {self.node_id} sends DAO to {self.parent.node_id} with prefix {self.prefix}')
+        # Send DAO message to parent
         if self.parent:
-            print(f'{self.env.now:.2f} {self.node_id} sends DAO to {self.parent.node_id} with prefix {self.prefix}')
+            log(f'{self.env.now:.2f} {self.node_id} sends DAO to {self.parent.node_id}, Prefix: {self.prefix}')
             yield self.env.process(self.parent.receive_dao(self.prefix, self))  # Ensure using 'yield'
     
     def receive_dao(self, prefix, child):
-        print(f'{self.env.now:.2f} {self.node_id} received DAO from {child.node_id}, Prefix: {prefix}')
-        if self.parent:
-            yield self.env.process(self.parent.receive_dao(prefix, self))  # Ensure using 'yield'
+        # Receive DAO message from child and forward to parent
+        if self.parent and self.parent != child:
+            child.prefix = prefix
+            self.env.process(self.parent.receive_dao(prefix, self))
+            log(f'{self.env.now:.2f} {self.node_id} received DAO from {child.node_id}, Prefix: {prefix}')
 
         yield self.env.timeout(0)  # Ensure this function is a generator
 
     def calculate_distance(self, other_position):
+        # Calculate distance between this node and another node
         return ((self.position[0] - other_position[0])**2 + (self.position[1] - other_position[1])**2)**0.5
     
     def reset_trickle(self):
+        # Reset Trickle parameters
         self.I = self.Imin
         self.t = random.uniform(self.Imin, self.I)
 
     def trickle_timer(self):
+        # Trickle timer for sending DIS messages
         while True:
             yield self.env.timeout(self.t)
             if not self.neighbors:
-                self.send_dis()
+                yield self.env.process(self.send_dis())
+                log(f'{self.env.now:.2f} {self.node_id} sends DIS broadcast for neighbor discovery')
             self.I = min(self.I * 2, self.Imax)
             self.t = random.uniform(self.Imin, self.I)
+
+    def network_disruption(self):
+        # Simulate network disruption by removing all neighbors 
+        # 1/20 chances of network disruption every 5 seconds
+        while True:
+            yield self.env.timeout(self.t)
+            if random.random() < (1/100):
+                log(f'{self.env.now:.2f} {self.node_id} network disruption')
+                self.lost_neighbors.clear()
+
+                # Remove self from neighbors
+                for neighbor in self.neighbors:
+                    if self in neighbor.neighbors:
+                        neighbor.neighbors.remove(self)
+                
+                # Add all neighbors to lost_neighbors
+                for neighbor in self.neighbors:
+                    self.lost_neighbors.append(neighbor)
+                
+                # Clear neighbors
+                self.neighbors.clear()
+
+                # Re-Discover neighbors
+                yield self.env.process(self.discover_neighbors())
 
 def setup_environment(num_nodes, width, height):
     env = simpy.Environment()
     nodes = []
-
-    # Initialize a plot
-    plt.figure(figsize=(8, 8))
-    plt.title('Node Distribution in RPL Simulation')
-    plt.xlabel('Width (meters)')
-    plt.ylabel('Height (meters)')
 
     def create_nodes():
         for i in range(num_nodes):
@@ -160,7 +194,7 @@ def setup_environment(num_nodes, width, height):
             while not valid_position:
                 valid_position = True
                 for node in nodes:
-                    if node.calculate_distance(position) < MINIMUM_DISTANCE:
+                    if node.calculate_distance(position) < config["MINIMUM_DISTANCE"]:
                         valid_position = False
                         position = (round(random.uniform(0, width), 4), round(random.uniform(0, height), 4))
                         break
@@ -170,35 +204,47 @@ def setup_environment(num_nodes, width, height):
             env.process(node.discover_neighbors())
             env.process(node.send_dio())
             env.process(node.trickle_timer())
-            print(f'{env.now:.2f} {node.node_id} created at {position}')
+            env.process(node.network_disruption())
+            log(f'{env.now:.2f} {node.node_id} created at {node.position}')
             
-            # Plot the node position
-            c = (random.random(), random.random(), random.random())
-            plt.scatter(*position, color=c, label=f'n{i}', s=15)
-            # plt.text(position[0], position[1] - 3, f'n{i}', fontsize=7, ha='center', va='bottom', color=c)
-            # plt.gca().add_artist(plt.Circle(position, CONNECTION_RANGE, color=c, fill=False, linestyle='--', linewidth=0.3, alpha=0.50))
-            
-            yield env.timeout(NODE_CREATION_INTERVAL)
+            yield env.timeout(config["NODE_CREATION_INTERVAL"])
 
     env.process(create_nodes())
     return env, nodes
 
 # Setup and start the simulation
 print('RPL Simulation')
-env, nodes = setup_environment(NUM_NODES, AREA_WIDTH, AREA_HEIGHT)
-env.run(until=60)  # Extend runtime to ensure all nodes are created and can act
+env, nodes = setup_environment(config["NUM_NODES"], config["AREA_WIDTH"], config["AREA_HEIGHT"])
+env.run(until=config["RUNTIME"])  # Extend runtime to ensure all nodes are created and can act
 
 
-# Draw Node Name and Their Prefix
+# Initialize a plot
+plt.figure(figsize=(8, 8))
+plt.title('Node Distribution in RPL Simulation')
+plt.xlabel('Width (meters)')
+plt.ylabel('Height (meters)')
+
 for node in nodes:
-    _text = f'{node.node_id.replace("Node", "N")} {node.prefix.replace("2001:db8::", "::")}'
-    plt.text(node.position[0], node.position[1] - 3, _text, fontsize=7, ha='center', va='bottom', color='black')
+    plt.scatter(node.position[0], node.position[1], color=node.color, label=node.node_id)
+
+    # Node Name
+    node_name = node.node_id.replace('Node', 'N') + '\n' + node.prefix.replace('2001:db8::', '::')
+    bottom_padding = int(config["AREA_HEIGHT"]) * 0.05
+    plt.text(node.position[0], node.position[1] - bottom_padding, node_name, fontsize=6, ha='center', va='bottom', color=node.color)
+
+    connected_neighbors = [neighbor for neighbor in node.neighbors if neighbor in nodes]
+    for neighbor in connected_neighbors:
+        plt.plot([node.position[0], neighbor.position[0]], [node.position[1], neighbor.position[1]], color='blue', alpha=0.75, linestyle='dotted', linewidth=0.75)
+
+    # Currently Lost Neighbors: lost_neighbors - neighbors
+    lost_neighbors = [neighbor for neighbor in node.lost_neighbors if neighbor not in connected_neighbors]
+    for neighbor in lost_neighbors:
+        plt.plot([node.position[0], neighbor.position[0]], [node.position[1], neighbor.position[1]], color='red', alpha=0.75, linestyle='dotted', linewidth=0.75)
 
 # Show plot
-plt.xlim(0, AREA_WIDTH)
-plt.ylim(0, AREA_HEIGHT)
+plt.xlim(0, config["AREA_WIDTH"])
+plt.ylim(0, config["AREA_HEIGHT"])
 plt.gca().set_aspect('equal', adjustable='box')
 plt.grid(False)
-# plt.show()
 plt.savefig('output.png', transparent=False, dpi=300)
 plt.close()
